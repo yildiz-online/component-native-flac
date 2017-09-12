5fb3316c534ffb32942ec686e131fcc71ed3d1d6
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2017-09-12 17:03:57 +0200

    [YIL-329] Flac native files.

---
+++ src/main/scripts/win32/build.sh
@@ -0,0 +1,13 @@
+cmake ../../c++ -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=FALSE -DCMAKE_COLOR_MAKEFILE=on -DCMAKE_INSTALL_PREFIX="../../../../target/classes/win32" -DCMAKE_TOOLCHAIN_FILE=mingw-toolchain.cmake
+
+make install
+r1=$?
+make clean
+
+rm -R CMakeFiles
+rm CMakeCache.txt
+rm cmake_install.cmake
+rm Makefile
+rm install_manifest.txt
+
+return $r1
